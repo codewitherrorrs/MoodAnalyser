72153a810376bdab00c375ad9272e8c29ec65f3c
“codewitherrors” <codewitherrors1@gmail.com>
2023-05-21 13:35:43 +0530

    Add : Analyse Mood Using Exception Handle

--- MoodAnalyser/Program.cs
+++ MoodAnalyser/Program.cs
@@ -10,16 +10,16 @@ namespace MoodAnalyser
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    MoodAnalyse moodAnalyse = new MoodAnalyse();
-            //    string mood = moodAnalyse.AnalyseMood("I am in Null Mood");
-            //    Console.WriteLine($"Mood is : {mood}");
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.WriteLine($"Exception is : {ex.Message}"); 
-            //} 
+            try
+            {
+                MoodAnalyse moodanalyse = new MoodAnalyse();
+                string mood = moodanalyse.AnalyseMood("I am in Null mood");
+                Console.WriteLine($"mood is : {mood}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"exception is : {ex.Message}");
+            }
         }
     }
 }
